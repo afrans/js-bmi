ce456e961f2674a207636b7c8ad096ff39b1df93
afrans <aluno@LTI2-030190.local.inatel.br>
2015-05-30 16:23:19 -0300

    Add css e calculo do imc

--- asset/js/BMIController.js
+++ asset/js/BMIController.js
@@ -7,19 +7,28 @@ var BMIController = {
     setForm: function(){
         var form = document.querySelector('form');
         form.addEventListener('submit', function(event){
-            alert('Form submitted');
+            BMIController.calculateBMI(form);
+            //alert('Form submitted');
             //it is necessary to avoid form submition
             event.preventDefault();
         });
     },
     
     calculateBMI: function(form){
+        var 
+        weight = parseFloat(form.weight.value),
+        height = parseFloat(form.height.value),    
+        result = 0;    
+        result = BMIService.getIndex(weight, height);
+        //BMIService.getDescription(weight, height);
         
+        BMIController.showResult(result);
     },
     
     showResult: function(result){
-        
-    }
+        var spanResult = document.querySelector('.result');
+        spanResult.innerHTML = result.toFixed(2);
+    },
     
 };
 
